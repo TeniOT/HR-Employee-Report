6e3e1c27365393e78553b891476f7e04efee9647
Teni Olufon <164643376+TeniOT@users.noreply.github.com>
2024-04-28 17:50:10 +0100

    Add files via upload

---
+++ HR Employee dataset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+
+# In[4]:
+
+
+# Read in the data
+
+df = pd.read_csv(r'C:\Users\toluf\OneDrive\Desktop\DA - Project\SQL + PowerBI\HR data set.csv')
+
+
+# In[5]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
