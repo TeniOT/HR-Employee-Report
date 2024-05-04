fc6de23956f3de23a24fc6f22581a9027b332acb
Teni Olufon <164643376+TeniOT@users.noreply.github.com>
2024-05-04 16:04:57 +0100

    EDA via Python

--- HR Employee dataset.py
+++ HR Employee dataset.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import pandas as pd
@@ -21,6 +21,84 @@ df = pd.read_csv(r'C:\Users\toluf\OneDrive\Desktop\DA - Project\SQL + PowerBI\HR
 
 
 df.head()
+
+
+# # EXPLORATORY DATA ANALYSIS
+
+# In[6]:
+
+
+df.shape
+
+
+# In[7]:
+
+
+df.columns
+
+
+# In[8]:
+
+
+df.info
+
+
+# In[9]:
+
+
+df.isnull().sum()
+
+
+# In[12]:
+
+
+df
+
+
+# In[11]:
+
+
+#What is the gender breakdown of employees in the company?
+df['gender'].value_counts()
+
+
+# In[13]:
+
+
+#What is the race/ethnicity breakdown of employees in the company?
+df['race'].value_counts()
+
+
+# In[15]:
+
+
+#What is the age distribution of employees in the company?
+df['jobtitle'].value_counts()
+
+
+# In[14]:
+
+
+#How many employees work at headquarters versus remote locations?
+df['location'].value_counts()
+
+
+# In[23]:
+
+
+df.describe()
+
+
+# In[28]:
+
+
+df.nunique()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
